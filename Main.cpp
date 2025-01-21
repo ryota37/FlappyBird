60a9e528b2b25201d46f0c3a1eb4c83398141571
ryota37 <ryota.nakahama@access-company.com>
2025-01-21 21:49:52 +0900

    Minimum impl of claypipe scroll

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,42 @@
-﻿# include <Siv3D.hpp> // Siv3D v0.6.15
+﻿# include <Siv3D.hpp> // Siv3Dライブラリをインクルード
 
 void Main()
 {
+	// ウィンドウ設定
+	Window::Resize(800, 600);
 
+	// 色付き四角の定義
+	struct ColoredRect
+	{
+		Rect rect;
+		ColorF color;
+
+		void draw() const
+		{
+			rect.draw(color);
+		}
+	};
+
+	// 背景画像
+	ColoredRect clayPipe{Rect(800,0,70,300), Palette::Green };
+
+	// スクロール用変数
+
+
+	// ゲームループ
 	while (System::Update())
 	{
+		// 背景をスクロールさせる
+		double deltaTime = Scene::DeltaTime();
+		clayPipe.rect.x -= (deltaTime * 20);
+
+		if (clayPipe.rect.x == 0)
+		{
+			clayPipe.rect.x = 800;
+		}
+
+		// 背景の描画
+		clayPipe.draw();
+
 	}
 }
-
