8f238436d0f6f5ae54e5350fccca8cff023e73c2
ryota37 <ryota.nakahama@access-company.com>
2025-01-27 22:30:44 +0900

    Write comment for future implementation plan

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,10 @@
 
 void Main()
 {
-	// ウィンドウ設定
+	// Window setting
 	Window::Resize(800, 600);
 
-	// 色付き四角の定義
+	// Define colored rect
 	struct ColoredRect
 	{
 		Rect rect;
@@ -18,33 +18,44 @@ void Main()
 		}
 	};
 
-	// 背景画像
+	// Instaniate the claypipe
 	ColoredRect clayPipe{Rect(800,0,70,300), Palette::Green };
-
-	// スクロール用変数
-
 
 	// Instantiate the bird
 	Bird bird{ Scene::CenterF().x, Scene::CenterF().y, 20.0 };
 
-	// ゲームループ
+	// MainLoop
 	while (System::Update())
 	{
-		// 背景をスクロールさせる
-		double deltaTime = Scene::DeltaTime();
-		clayPipe.rect.x -= (deltaTime * 20);
 
+		// Update position of the claypipe
+		// The code below should be extracted to Claypipe class.
+		clayPipe.rect.x -= (Scene::DeltaTime() * 20);
 		if (clayPipe.rect.x == 0)
 		{
 			clayPipe.rect.x = 800;
 		}
 
-		// 背景の描画
-		clayPipe.draw();
 
-		// Rendering of the bird
-		bird.update();
-		bird.draw();
+		// Switch the code to execute depending on the GameState.
+		// NOTE: wanna implement with the strategy pattern
+
+
+		// The title screen shown before playing the game.
+
+		// The process that carried out during the game play
+
+			// Rendering of the claypipe
+			clayPipe.draw();
+			// Rendering of the bird
+			bird.update();
+			bird.draw();
+			// Send collide info to ManageGameState::switchGameState()
+
+			// Send the information that the bird passes the claypipe to not-yet desined class(DisplayCount?)
+
+
+		// The game over screen shown when the bird died
 
 	}
 }
