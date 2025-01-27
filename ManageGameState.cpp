ab277a65251bd041e9318aaaf5f377417d9419e9
ryota37 <ryota.nakahama@access-company.com>
2025-01-27 22:32:27 +0900

    (WIP) Implementing a class that manages the game state

---
+++ ManageGameState.cpp
@@ -0,0 +1,33 @@
+﻿# include <Siv3D.hpp>
+# include "ManageGameState.h"
+
+ManageGameState::ManageGameState()
+	:gamestate(GameState::BeforePlaying) { };
+
+enum class GameState {
+	BeforePlaying,
+	Playing,
+	GameOver
+};
+
+GameState ManageGameState::getGameState()
+{
+	return gamestate;
+}
+
+void ManageGameState::setGameState(GameState updated_gamestate)
+{
+	gamestate = updated_gamestate;
+}
+
+GameState ManageGameState::switchGameMode()
+{
+	// when recieve enterkey press, turn GameState to Playing
+
+	// when recieve collide info, turn GameState to GameOver
+
+	// when recieve retry button press, turn GameState to Playing
+
+	// when recieve title button press, turn GameState to BeforePlaying
+}
+
---
+++ ManageGameState.h
@@ -0,0 +1,11 @@
+﻿# include <Siv3D.hpp>
+
+class ManageGameState
+{
+private:
+	enum class GameState gamestate;
+public:
+	GameState getGameState();
+	void setGameState(GameState gamestate);
+	GameState switchGameMode();
+};
