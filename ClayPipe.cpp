c958dddece60c02d48dc8b20ed686df4efd91a32
ryota37 <ryota.nakahama@access-company.com>
2025-01-30 23:14:16 +0900

    (WIP) extract ClayPipe to a class

---
+++ ClayPipe.cpp
@@ -0,0 +1,31 @@
+﻿# include <Siv3D.hpp>
+
+// Define colored rect
+struct ColoredRect
+{
+	Rect rect;
+	ColorF color;
+
+	void draw() const
+	{
+		rect.draw(color);
+	}
+};
+
+class ClayPipe : public ColoredRect
+{
+private:
+
+public:
+	ClayPipe(Rect rect = Rect(800, 0, 70, 300), ColorF color = Palette::Green) : ColoredRect{rect,color} {};
+
+	void update()
+	{
+		// Update position of the claypipe
+		rect.x -= (Scene::DeltaTime() * 20);
+		if (rect.x == 0)
+		{
+			rect.x = 800;
+		}
+	}
+};
--- Main.cpp
+++ Main.cpp
@@ -2,40 +2,22 @@
 # include <memory>
 # include "Bird.h"
 # include "GameState.cpp"
+# include "ClayPipe.cpp"
 
 void Main()
 {
 	// Window setting
 	Window::Resize(800, 600);
 
-	// Define colored rect
-	struct ColoredRect
-	{
-		Rect rect;
-		ColorF color;
-
-		void draw() const
-		{
-			rect.draw(color);
-		}
-	};
-
-	// Instaniate the claypipe
-	ColoredRect clayPipe{Rect(800,0,70,300), Palette::Green };
-
-	// Instantiate the bird
+	// Instantiation
 	Bird bird{ Scene::CenterF().x, Scene::CenterF().y, 20.0 };
-
-
+	ClayPipe clayPipe;
 
 	GameContext game;
-
 
 	// MainLoop
 	while (System::Update())
 	{
-
-
 		if (KeySpace.down())
 		{
 			game.startGame();
@@ -47,32 +29,15 @@ void Main()
 
 		game.exec();
 
+		// Rendering of the claypipe
+		clayPipe.update();
+		clayPipe.draw();
+		// Rendering of the bird
+		bird.update();
+		bird.draw();
+		// Send collide info to ManageGameState::switchGameState()
 
-		// Update position of the claypipe
-		// The code below should be extracted to Claypipe class.
-		clayPipe.rect.x -= (Scene::DeltaTime() * 20);
-		if (clayPipe.rect.x == 0)
-		{
-			clayPipe.rect.x = 800;
-		}
-
-
-		// Switch the code to execute depending on the GameState.
-		// NOTE: wanna implement with the strategy pattern
-
-
-
-		// The process that carried out during the game play
-
-			// Rendering of the claypipe
-			clayPipe.draw();
-			// Rendering of the bird
-			bird.update();
-			bird.draw();
-			// Send collide info to ManageGameState::switchGameState()
-
-			// Send the information that the bird passes the claypipe to not-yet desined class(DisplayCount?)
-
+		// Send the information that the bird passes the claypipe to not-yet desined class(DisplayCount?)
 
 		// The game over screen shown when the bird died
 
