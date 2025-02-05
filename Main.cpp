b64421b7d96a890583dcabafc857d1408ac2d364
ryota37 <ryota.nakahama@access-company.com>
2025-02-05 18:23:16 +0900

    Implement bird collision and gameover

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,9 @@
 
 void Main()
 {
+	// Font information
+	const Font minifont{ FontMethod::MSDF, 24, Typeface::Bold };
+
 	// Define colored rect
 	struct ColoredRect
 	{
@@ -21,24 +24,47 @@ void Main()
 	// Instantiate the bird
 	Bird bird{ Scene::CenterF().x, Scene::CenterF().y, 20.0 };
 
+	// ManageGameOver
+	bool isGameOver = false;
+
 	// MainLoop
 	while (System::Update())
 	{
 
-		// Update position of the claypipe
-		clayPipe.rect.x -= (Scene::DeltaTime() * 20);
-		if (clayPipe.rect.x == 0)
+		if (!isGameOver)
 		{
-			clayPipe.rect.x = 800;
+			// Update position of the claypipe
+			clayPipe.rect.x -= (Scene::DeltaTime() * 20);
+			if (clayPipe.rect.x == 0)
+			{
+				clayPipe.rect.x = 800;
+			}
+
+			// Rendering of the claypipe
+			clayPipe.draw();
+			// Rendering of the bird
+			bird.update();
+			bird.draw();
+
+			// The game over screen shown when the bird died
+			if (bird.collide())
+			{
+				isGameOver = true;
+			}
+			if (bird.intersects(clayPipe.rect))
+			{
+				isGameOver = true;
+			}
 		}
-
-		// Rendering of the claypipe
-		clayPipe.draw();
-		// Rendering of the bird
-		bird.update();
-		bird.draw();
-
-		// The game over screen shown when the bird died
-
+		else
+		{
+			minifont(U"GameOver!").drawAt(Scene::Center());
+			if (KeyEnter.pressed())
+			{
+				clayPipe = { Rect(800,0,70,300), Palette::Green };
+				bird = { Scene::CenterF().x, Scene::CenterF().y, 20.0 };
+				isGameOver = false;
+			}
+		}
 	}
 }
