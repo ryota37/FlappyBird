9e7310c0d4913202da6ed5cedca23e80c65648b5
ryota37 <ryota.nakahama@access-company.com>
2025-02-05 18:04:59 +0900

    Deleted unnecessary comments * gave up implementation of interface

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,6 @@
 
 void Main()
 {
-	// Window setting
-	Window::Resize(800, 600);
-
 	// Define colored rect
 	struct ColoredRect
 	{
@@ -29,31 +26,17 @@ void Main()
 	{
 
 		// Update position of the claypipe
-		// The code below should be extracted to Claypipe class.
 		clayPipe.rect.x -= (Scene::DeltaTime() * 20);
 		if (clayPipe.rect.x == 0)
 		{
 			clayPipe.rect.x = 800;
 		}
 
-
-		// Switch the code to execute depending on the GameState.
-		// NOTE: wanna implement with the strategy pattern
-
-
-		// The title screen shown before playing the game.
-
-		// The process that carried out during the game play
-
-			// Rendering of the claypipe
-			clayPipe.draw();
-			// Rendering of the bird
-			bird.update();
-			bird.draw();
-			// Send collide info to ManageGameState::switchGameState()
-
-			// Send the information that the bird passes the claypipe to not-yet desined class(DisplayCount?)
-
+		// Rendering of the claypipe
+		clayPipe.draw();
+		// Rendering of the bird
+		bird.update();
+		bird.draw();
 
 		// The game over screen shown when the bird died
 
